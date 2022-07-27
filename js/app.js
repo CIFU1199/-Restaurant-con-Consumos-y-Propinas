375219a6907b79984ea38d12467c9b8d0b4b8501
CIFU1199 <edwkamilo@gmail.com>
2022-07-27 11:33:55 -0500

    mostrar contenido al pasar la validacion

--- js/app.js
+++ js/app.js
@@ -13,9 +13,42 @@ function guardarCliente(){
 
     const camposVacios = [mesa, hora].some(campo => campo ==='');
     if(camposVacios){
-        console.log('Si hay almenos un campo vacio');
-    }else{
-        console.log('Todos los campos estan llenos');
+        //verificar si ya hay una alerta 
+        const ejAlerta = document.querySelector('.invalid-feedback');
+
+        if(!ejAlerta){
+            const alerta = document.createElement('div');
+            alerta.classList.add('invalid-feedback','d-block','text-center');
+
+            alerta.textContent = 'Todos los campos son obligatorios';
+
+            document.querySelector('.modal-body form').appendChild(alerta);
+
+            setTimeout(() => {
+                alerta.remove();
+            }, 3000);
+        }
+        return;   
     }
+    //asignar datos del formulario a cliente
+    cliente = {...cliente, mesa, hora};
+    //console.log(cliente);
+
+    //ocultar modal
+
+    const modalFormulario = document.querySelector('#formulario');
+    const modalBootstrap = bootstrap.Modal.getInstance(modalFormulario);
+    modalBootstrap.hide();
+    //mostrar las secciones ocultas
+    mostrarSecciones();
+    //obtener platillos del json-server 
 
 }
+
+ function mostrarSecciones(){
+    //querySelector cuando solo cuando la propiedad a manejar es unica y querySelectorAll cuando la funcion se aplica a varios 
+    const seccionesOcultas = document.querySelectorAll('.d-none');
+    seccionesOcultas.forEach(seccion => seccion.classList.remove('d-none'));
+    
+
+ }
