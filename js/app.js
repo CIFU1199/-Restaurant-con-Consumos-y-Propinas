c4fa2182ea7f20aa60cb6d8e062241026b64ebf0
CIFU1199 <edwkamilo@gmail.com>
2022-07-27 15:08:48 -0500

    Detectar si un Producto ya esta en el carrito

--- js/app.js
+++ js/app.js
@@ -87,12 +87,28 @@ function guardarCliente(){
         categoria.classList.add('col-md-3');
         categoria.textContent = categorias[platillo.categoria];
 
+        const inputCantidad = document.createElement('input');
+        inputCantidad.type = 'number';
+        inputCantidad.min = 0;
+        inputCantidad.value = 0;
+        inputCantidad.id = `Producto-${platillo.id}`; 
+        inputCantidad.classList.add('form-control');
 
+        //funcion que detecta la cantidad de platillos pedidos
+        inputCantidad.onchange =  function () {
+            const cantidad = parseInt (inputCantidad.value);
+            agregarPlatillo({...platillo, cantidad});
+        };
         
+        const agregar = document.createElement('div');
+        agregar.classList.add('col-md-2');
+        agregar.appendChild(inputCantidad)
+
         row.appendChild(nombre);
 
         row.appendChild(precio);
         row.appendChild(categoria);
+        row.appendChild(agregar);
         contenido.appendChild(row);
         
         //console.log(platillo); 
@@ -100,4 +116,16 @@ function guardarCliente(){
 
     //console.log(platillos);
 
+ }
+
+ function agregarPlatillo(producto) {
+    let { pedido } = cliente;
+
+    if(producto.cantidad > 0){
+        cliente.pedido = [...pedido.producto];        
+    }else{
+        console.log('No es mayor a 0');
+    }
+
+    console.log(cliente.pedido);
  }
